633d1dfedc53fe22b28d3e22fce42acece6d55d9
teddyhn <tngo97@gmail.com>
2019-09-20 08:29:00 -0700

    axiosWithAuth module and PrivateRoute component implemented

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+      username: '',
+      password: ''
+  });
+
+  const handleChange = evt => {
+      setCredentials({ ...credentials, [evt.target.name]: evt.target.value })
+  };
+
+  const login = evt => {
+      evt.preventDefault();
+      axiosWithAuth()
+        .post('/login', credentials)
+        .then(res => {
+          localStorage.setItem('token', res.data.payload);
+          props.history.push('/bubbles');
+        })
+        .catch(err => console.log(err));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+      <div className="login-page">
+          <h1>Bubbles: Log in</h1>
+          <Form className="react-bootstrap-form" onSubmit={login}>
+              <Form.Group controlId="formBasicUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control 
+                      type="text" 
+                      name="username"
+                      value={credentials.username}
+                      onChange={evt => handleChange(evt)}
+                  />
+              </Form.Group>
 
-export default Login;
+              <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control 
+                      type="password"
+                      name="password"
+                      value={credentials.password}
+                      onChange={evt => handleChange(evt)}
+                  />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                  Submit
+              </Button>
+          </Form>
+      </div>
+  )
+}
+
+export default Login
---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+import { } from "../actions";
+  
+const initialState = {
+    colors: [],
+    isFetching: false,
+    error: ""
+};
+  
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
